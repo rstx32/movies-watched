2b0856466fea9fcd69fc902a142d11d85acc6e24
restu indera <rstx32@gmail.com>
2022-11-11 09:04:11 +0700

    add base core

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 import express from 'express'
-import { getAllMovies, getAllSeries, insertNewMovies } from './app/route.js'
+import {
+  getAllMovies,
+  getAllSeries,
+  insertNewMovies,
+  insertNewSeries,
+} from './app/route.js'
 
 const app = express()
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  res.render('home')
+})
 app.get('/movies', getAllMovies)
 app.post('/movies', insertNewMovies)
 app.get('/series', getAllSeries)
+app.post('/series', insertNewSeries)
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`app running on port ${process.env.APP_PORT}`)
