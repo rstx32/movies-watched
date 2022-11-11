6f401e4e03a574362a08ac5367d2ffa046c9e203
restu indera <rstx32@gmail.com>
2022-11-11 09:03:28 +0700

    add get all items & insert new items

--- app/route.js
+++ app/route.js
@@ -1,15 +1,29 @@
-import { movies, series } from './file.js'
+import { moviesList, seriesList, insertMovies, insertSeries } from './file.js'
 
+// get all movies
 const getAllMovies = (req, res) => {
-  res.send(JSON.parse(movies))
+  res.send(JSON.parse(moviesList))
 }
 
+// insert new movies
 const insertNewMovies = (req, res) => {
-    
+  const success = insertMovies(req.body)
+
+  if (success) res.send('success add new movie')
+  else res.send('movie already added')
 }
 
+// get all series
 const getAllSeries = (req, res) => {
-  res.send(JSON.parse(series))
+  res.send(JSON.parse(seriesList))
 }
 
-export { getAllMovies, getAllSeries, insertNewMovies }
+// insert new series
+const insertNewSeries = (req, res) => {
+  const success = insertSeries(req.body)
+
+  if (success) res.send('success add new series')
+  else res.send('series already added')
+}
+
+export { getAllMovies, getAllSeries, insertNewMovies, insertNewSeries }
